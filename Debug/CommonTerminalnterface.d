f9e43a12e84e0dcaa75ba2cc024035de27c8bdc3
Sean Woerner <woerner99@gmail.com>
2021-02-23 17:00:28 -0600

    Updated terminal interface using new libraries for gpio and uart given this semester

---
+++ Debug/CommonTerminalnterface.d
@@ -0,0 +1,37 @@
+# FIXED
+
+CommonTerminalnterface.obj: ../CommonTerminalnterface.c
+CommonTerminalnterface.obj: E:/Program\ Files\ (x86)/Texas\ Insturments/CCS1010/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/stdint.h
+CommonTerminalnterface.obj: E:/Program\ Files\ (x86)/Texas\ Insturments/CCS1010/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/_ti_config.h
+CommonTerminalnterface.obj: E:/Program\ Files\ (x86)/Texas\ Insturments/CCS1010/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/linkage.h
+CommonTerminalnterface.obj: E:/Program\ Files\ (x86)/Texas\ Insturments/CCS1010/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/_stdint40.h
+CommonTerminalnterface.obj: E:/Program\ Files\ (x86)/Texas\ Insturments/CCS1010/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/sys/stdint.h
+CommonTerminalnterface.obj: E:/Program\ Files\ (x86)/Texas\ Insturments/CCS1010/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/sys/cdefs.h
+CommonTerminalnterface.obj: E:/Program\ Files\ (x86)/Texas\ Insturments/CCS1010/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/sys/_types.h
+CommonTerminalnterface.obj: E:/Program\ Files\ (x86)/Texas\ Insturments/CCS1010/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/machine/_types.h
+CommonTerminalnterface.obj: E:/Program\ Files\ (x86)/Texas\ Insturments/CCS1010/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/machine/_stdint.h
+CommonTerminalnterface.obj: E:/Program\ Files\ (x86)/Texas\ Insturments/CCS1010/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/sys/_stdint.h
+CommonTerminalnterface.obj: E:/Program\ Files\ (x86)/Texas\ Insturments/CCS1010/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/stdbool.h
+CommonTerminalnterface.obj: E:/Program\ Files\ (x86)/Texas\ Insturments/CCS1010/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/string.h
+CommonTerminalnterface.obj: ../tm4c123gh6pm.h
+CommonTerminalnterface.obj: ../clock.h
+CommonTerminalnterface.obj: ../gpio.h
+CommonTerminalnterface.obj: ../uart0.h
+
+../CommonTerminalnterface.c: 
+E:/Program\ Files\ (x86)/Texas\ Insturments/CCS1010/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/stdint.h: 
+E:/Program\ Files\ (x86)/Texas\ Insturments/CCS1010/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/_ti_config.h: 
+E:/Program\ Files\ (x86)/Texas\ Insturments/CCS1010/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/linkage.h: 
+E:/Program\ Files\ (x86)/Texas\ Insturments/CCS1010/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/_stdint40.h: 
+E:/Program\ Files\ (x86)/Texas\ Insturments/CCS1010/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/sys/stdint.h: 
+E:/Program\ Files\ (x86)/Texas\ Insturments/CCS1010/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/sys/cdefs.h: 
+E:/Program\ Files\ (x86)/Texas\ Insturments/CCS1010/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/sys/_types.h: 
+E:/Program\ Files\ (x86)/Texas\ Insturments/CCS1010/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/machine/_types.h: 
+E:/Program\ Files\ (x86)/Texas\ Insturments/CCS1010/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/machine/_stdint.h: 
+E:/Program\ Files\ (x86)/Texas\ Insturments/CCS1010/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/sys/_stdint.h: 
+E:/Program\ Files\ (x86)/Texas\ Insturments/CCS1010/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/stdbool.h: 
+E:/Program\ Files\ (x86)/Texas\ Insturments/CCS1010/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/string.h: 
+../tm4c123gh6pm.h: 
+../clock.h: 
+../gpio.h: 
+../uart0.h: 
---
+++ Debug/subdir_vars.mk
@@ -0,0 +1,53 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../tm4c123gh6pm.cmd 
+
+C_SRCS += \
+../Terminal_lnterface.c \
+../clock.c \
+../gpio.c \
+../tm4c123gh6pm_startup_ccs.c \
+../uart0.c 
+
+C_DEPS += \
+./Terminal_lnterface.d \
+./clock.d \
+./gpio.d \
+./tm4c123gh6pm_startup_ccs.d \
+./uart0.d 
+
+OBJS += \
+./Terminal_lnterface.obj \
+./clock.obj \
+./gpio.obj \
+./tm4c123gh6pm_startup_ccs.obj \
+./uart0.obj 
+
+OBJS__QUOTED += \
+"Terminal_lnterface.obj" \
+"clock.obj" \
+"gpio.obj" \
+"tm4c123gh6pm_startup_ccs.obj" \
+"uart0.obj" 
+
+C_DEPS__QUOTED += \
+"Terminal_lnterface.d" \
+"clock.d" \
+"gpio.d" \
+"tm4c123gh6pm_startup_ccs.d" \
+"uart0.d" 
+
+C_SRCS__QUOTED += \
+"../Terminal_lnterface.c" \
+"../clock.c" \
+"../gpio.c" \
+"../tm4c123gh6pm_startup_ccs.c" \
+"../uart0.c" 
+
+
